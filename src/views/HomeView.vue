ca068a8ce84266a10bc5f4572ae58dfb7926bf21
User <maxandreev27@gmail.com>
2025-07-18 17:49:37 +0300

    Remove all standard components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,11 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+        <h1>This is an Home page</h1>
     </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Options({
-    components: {
-        HelloWorld,
-    },
-})
-export default class HomeView extends Vue {}
+<script>
+export default {
+    name: 'HomeView',
+};
 </script>
